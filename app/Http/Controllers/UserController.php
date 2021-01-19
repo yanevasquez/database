69628cca6a5b1b66c5789e14ab1b1adfacfc0ec7
Yane <yanelisset4@gmail.com>
2021-01-18 23:56:05 -0300

    feat(users): add meta column as json type for testing

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,9 +18,12 @@ class UserController extends Controller
 
     public function index()
     {
-        $users = $this->users->all();
-        //$users = $this->users->where('name', 'Derick Oberbrunner')->get();
-        //$users = $this->users->select('id')->distinct()->get();
+        // $users = $this->users->all();
+        // $users = $this->users->where('name', 'Derick Oberbrunner')->get();
+        // $users = $this->users->select('id')->distinct()->get();
+        // $users = $this->users->where('meta->settings->site_language', 'en')->get();
+        $users = $this->users->whereJsonContains('meta->skills', 'Laravel')->get();
+            dd($users);
         return view('index', compact('users'));
     }
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,5 +24,13 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+        'meta' => [
+            'settings' => [
+                'site_background' => 'black',
+                'site_language' => 'en',
+            ],
+            'skills' => $faker->randomElements(['Laravel', 'PHP 8', 'Vue', 'HTML 5', 'CSS', 'ReactJS'], mt_rand(1, 6)),
+            'gender' => $faker->randomElement(['Male', 'Female', 'Other ;)'])
+        ]
     ];
 });
